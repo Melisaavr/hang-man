7ed356b8e69d9723308215f33b7389a9a4d59828
Melisaavr <melisadayicikk@gmail.com>
2023-10-16 20:02:52 +0100

    user input

--- run.py
+++ run.py
@@ -14,34 +14,33 @@ print("4. You have a limited number of incorrect guesses before you lose the gam
 print("5. Try to guess the word before you run out of guesses!")
 print("6. Good luck and have fun!\n")
 
-import random 
+
+import random
 from words import words
+from hangman_visual import lives_visual_dict
 import string
 
 
 def get_valid_word(words):
-    word = random.choice(words) #randomly chooses something from the list
+    word = random.choice(words)  # randomly chooses something from the list
+    while '-' in word or ' ' in word:
+        word = random.choice(words)
+
+    return word.upper()
 
 
 def hangman():
-    word= get_valid_word(words)
-    word_letters = set(word) # letters in the word
+    word = get_valid_word(words)
+    word_letters = set(word)  # letters in the word
     alphabet = set(string.ascii_uppercase)
-    used_letters = set() # what the user has guessed
+    used_letters = set()  # what the user has guessed
+
+    lives = 7
 
     # getting user input
-    user_letter = input('Guess a letter: ').upper()
-    if user_letter in alphabet - used_letters:
-        used_letters.add(user_letter)
-    if user_letter in word_letters:
-        word_letters.remove(user_letter)
+    while len(word_letters) > 0 and lives > 0:
+        # letters used
+        # ' '.join(['a', 'b', 'cd']) --> 'a b cd'
+        print('You have', lives, 'lives left and you have used these letters: ', ' '.join(used_letters))
 
-    elif user_letter in used_letters:
-        print('You have already used that character. Please try again!')
-    else:
-        print('Invalid character. Please try again!')
-
-
-user_input = input('Type something')
-print(user_input)
-
+      
